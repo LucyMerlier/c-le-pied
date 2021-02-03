b139d08d978207a5f4d09dde6416ea4dd6ba69ac
LucyMerlier <lucymerlier@gmail.com>
2021-02-03 19:23:53 +0100

    added script for like button

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,1 +1,27 @@
 require('bootstrap');
+
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+
+document.querySelector('#like').addEventListener('click', likePicture);
+
+function likePicture(event)
+{
+    event.preventDefault();
+
+    let likeLink = event.currentTarget;
+    let link = likeLink.href;
+    fetch(link)
+        .then(res => res.json())
+        .then(function(res) {
+            let likeIcon = likeLink.firstElementChild;
+            if (res.hasLiked) {
+                likeIcon.classList.remove('far');
+                likeIcon.classList.add('fas');
+            } else {
+                likeIcon.classList.remove('fas');
+                likeIcon.classList.add('far');
+            }
+        })
+    ;
+}
